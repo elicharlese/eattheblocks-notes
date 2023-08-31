62010dcb126be53dc69c924c0ff25fba59c8e384
elicharlese <elicharles.e@gmail.com>
2023-08-31 18:06:06 +0000

    Changes

--- End-To-End-Developer/3/thirdweb-contracts/contracts/MyContract.sol
+++ End-To-End-Developer/3/thirdweb-contracts/contracts/MyContract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
 // import "@thirdweb-dev/contracts/extension/DelayReveal.sol";
 
-contract MyContract is ERC721Base, { //TODO: Add DelayedReveal to the contracts implicits
+contract MyContract is ERC721Base { //TODO: Add DelayedReveal to the contracts implicits
 
     constructor(
         address _defaultAdmin,
