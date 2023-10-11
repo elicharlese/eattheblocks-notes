f0c315e8022f74bc730137b17ea65b7907fa4f82
Restyled.io <commits@restyled.io>
2023-10-11 07:54:28 +0000

    Restyled by prettier

--- End-To-End-Developer/2.2/hardhat.config.js
+++ End-To-End-Developer/2.2/hardhat.config.js
@@ -2,15 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork : "mainnet",
-  networks : {
-    hardhat : {},
-    goerli : {
-      url : "https://goerli.infura.io/v3/52fa75ec651f423a88b6d9f3d7fff6aa",
-      accounts : [ "" ] // TODO: Add Private keys for goerli wallet
+  defaultNetwork: "mainnet",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: "https://goerli.infura.io/v3/52fa75ec651f423a88b6d9f3d7fff6aa",
+      accounts: [""], // TODO: Add Private keys for goerli wallet
     },
   },
   // Verifying your smart contract
-  etherscan : {apiKey : ""},
-  solidity : "0.8.19",
+  etherscan: { apiKey: "" },
+  solidity: "0.8.19",
 };
